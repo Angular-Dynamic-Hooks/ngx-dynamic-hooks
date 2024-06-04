569b8a625ba3a38ebc75b077b205e58dfb4b3387
Marvin <Mvin@live.de>
2024-06-04 21:02:22 +0200

    Improved sidebar scroll highlight logic

--- docs/js/widgets/sidebarSections.ts
+++ docs/js/widgets/sidebarSections.ts
@@ -1,17 +1,19 @@
 import { GenericWidgetController, Widget } from "../widgetBootstrap";
 
 export class SidebarSectionsWidget implements Widget {
-  static selector: string = '.sidebar-section.hasChildren';
+  static selector: string = '.sidebar-section.active.hasChildren';
   public section: HTMLElement|null = null;
   public toggleButtonElement: HTMLElement|null = null;
   public articleElement: HTMLElement|null = null;
-  public titleElements: NodeListOf<HTMLElement>|null = null;
+  public sidebarTitleElements: NodeListOf<HTMLElement>|null = null;
+  public articleTitleElements: NodeListOf<HTMLElement>|null = null;
 
   onMount(hostElement: Element, data: {[key: string]: any}, controller: GenericWidgetController) {
     this.section = hostElement as HTMLElement;
     this.toggleButtonElement = this.section!.querySelector('.sidebar-title-toggle');
     this.articleElement = document.querySelector('.article');
-    this.titleElements = this.articleElement?.querySelectorAll('h1, h2, h3, h4') || null;
+    this.sidebarTitleElements = this.section.querySelectorAll('.sidebar-ul .sidebar-link') || null;
+    this.articleTitleElements = this.articleElement?.querySelectorAll('.article > h1, .article > h2, .article > h3, .article > h4') || null;
 
     this.registerToggleButton();
     this.registerActiveSectionScrollListener();
@@ -32,39 +34,46 @@ export class SidebarSectionsWidget implements Widget {
   }
 
   private registerActiveSectionScrollListener() {
-    if (!this.section!.classList.contains('active') || !this.titleElements) {
+    if (!this.articleTitleElements || !this.sidebarTitleElements) {
       return;
     }
 
-    const visibilityStates: Map<string, boolean> = new Map();
-    for (const titleElement of this.titleElements) {
-      visibilityStates.set(titleElement.id, false);
+    const visibilityStates: {element: Element, link: string; visible: boolean}[] = [];
+    for (const sidebarTitleElement of this.sidebarTitleElements!) {
+      if (sidebarTitleElement.hasAttribute('href')) {
+        visibilityStates.push({
+          element: sidebarTitleElement,
+          link: sidebarTitleElement.getAttribute('href')!, 
+          visible: false
+        });
+      }
     }
 
     const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
       for (const entry of entries) {
-        visibilityStates.set(entry.target.id, entry.isIntersecting);
+        const anchorLink = `${ location.pathname.split('#')[0] + '#' + entry.target.id }`;
+        const sidebarTitleState = visibilityStates.find(state => state.link === anchorLink);
+        if (sidebarTitleState) {
+          sidebarTitleState.visible = entry.isIntersecting;
+        }
       }
 
-      const firstVisibleTitle = Array.from(visibilityStates).find(entry => entry[1] === true);
-      if (firstVisibleTitle) {
-        const selector = `.sidebar-ul .sidebar-link[href='${ location.pathname.split('#')[0] + '#' + firstVisibleTitle[0] }']`;
-        const activeChild = this.section!.querySelector(selector);
+      console.log(visibilityStates);
 
-        if (activeChild) {
+      const firstVisibleTitleState = visibilityStates.find(state => state.visible);
+      if (firstVisibleTitleState) {
           // Reset all
-          this.section!.querySelectorAll('.sidebar-ul .sidebar-link').forEach(element => element.closest('.sidebar-li')?.classList.remove('active'));
+          visibilityStates.forEach(state => state.element.closest('.sidebar-li')?.classList.remove('active'));
         
           // Set new title to active
-          activeChild?.closest('.sidebar-li')?.classList.add('active');
-        }
+          firstVisibleTitleState.element.closest('.sidebar-li')?.classList.add('active');
       }
     }, {
       rootMargin: "0px",
       threshold: 1.0,
     });
 
-    for (const titleElement of this.titleElements) {
+    for (const titleElement of this.articleTitleElements) {
       observer.observe(titleElement);
     }
   }
