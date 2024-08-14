a824d1141bb440820c94ba1a819ecf0a643300d5
Marvin <Mvin@live.de>
2024-08-14 18:13:41 +0200

    Moved docs to own repository

--- docs/js/misc.ts
+++
@@ -1,6 +0,0 @@
-export const initCopyrightDate = () => {
-  const copyrightDateElements = document.querySelectorAll('.copyright-date');
-  for (const copyrightDateElement of copyrightDateElements) {
-    copyrightDateElement.innerHTML = new Date().getFullYear().toString();
-  }
-}
